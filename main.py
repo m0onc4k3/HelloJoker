46cbedc2fa731cfcbb3bbd4257bf1d5005ba8b12
taggiogit <taggiogit@taggioserver.com>
2025-01-11 08:00:17 +0000

    Update main

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-
+from fastapi import FastAPI
+from pyjokes import get_joke
+app = FastAPI()
+@app.get("/")
+async def root():
+    random_joke = get_joke("en","neutral")
+    return {"random_joke":random_joke}
